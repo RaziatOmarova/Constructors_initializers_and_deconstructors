55d170d2432e4b4767e0c4c2a31e110e6b119219
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-10-28 02:34:48 +0300

    Добавьте файлы проекта.

---
+++ классы и объекты/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace классы_и_объекты
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Raziat = new Person("Разият", 16);
+            Raziat.Print();
+            Person Viсtor = new Person("Виктор", 16);
+            Raziat.Print();
+            Person Magomed = new Person("Магомед", 17);
+            Magomed.Print();
+            Person Abdulhakim = new Person("Абдулхаким", 20);
+            Abdulhakim.Print();
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+
+        public Person(string name, byte age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name} \nAge: {Age}");
+        }
+    }
+}
